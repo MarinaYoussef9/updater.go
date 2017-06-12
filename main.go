d622d20a15ddc67a58b62e376520d6a6471014bd
Marco Younan <xmarcoied@gmail.com>
2017-06-12 01:47:59 +0100

    Parsing GET request

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strings"
 	"net/http"
 )
 
@@ -12,7 +14,17 @@ func status_ascHandler(w http.ResponseWriter, r *http.Request) {
 	  http.ServeFile(w, r, "static/status.asc")
 
 }
+func home(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm() 
+    for k, v := range r.Form {
+        fmt.Print(k , ": ")
+        fmt.Println(strings.Join(v, ""))
+    }
+    fmt.Fprintf(w, "Hello VLC!")
+}
+
 func main() {
+	http.HandleFunc("/showoff", home) 
 	http.HandleFunc("/vlc/status", statusHandler)
 	http.HandleFunc("/vlc/status.asc", status_ascHandler)
 	log.Fatal(http.ListenAndServe(":80", nil))
