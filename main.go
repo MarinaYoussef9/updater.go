dcb6db684df56b2f8e14fe7f925ffeda05477bfa
Marco Younan <xmarcoied@gmail.com>
2017-06-14 17:38:39 +0100

    Storing all sent requests

--- main.go
+++ main.go
@@ -1,43 +1,118 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"strings"
-	"net/http"
-	"io/ioutil"
+    "os"
+    "bufio"
+    "bytes"
+    "io"
+    "fmt"
+    "net/http"
+    "strings"
+    "log"
+    "io/ioutil"
     "net/url"
+    "time"
 )
-
+func homepage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w , "What the fuck!")
+}
 func statusHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/status")
+    http.ServeFile(w, r, "static/status")
+    log.Println("status")
 }
 func status_ascHandler(w http.ResponseWriter, r *http.Request) {
-	  http.ServeFile(w, r, "static/status.asc")
-
+    http.ServeFile(w, r, "static/status.asc")
+    log.Println("status.asc")
 }
+
+func readLines(path string) (lines []string, err error) {
+    var (
+        file *os.File
+        part []byte
+        prefix bool
+    )
+    if file, err = os.Open(path); err != nil {
+        return
+    }
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+    buffer := bytes.NewBuffer(make([]byte, 0))
+    for {
+        if part, prefix, err = reader.ReadLine(); err != nil {
+            break
+        }
+        buffer.Write(part)
+        if !prefix {
+            lines = append(lines, buffer.String())
+            buffer.Reset()
+        }
+    }
+    if err == io.EOF {
+        err = nil
+    }
+    return
+}
+
+func writeLines(lines []string, path string) (err error) {
+    var (
+        file *os.File
+    )
+
+    if file, err = os.Create(path); err != nil {
+        return
+    }
+    defer file.Close()
+
+    //writer := bufio.NewWriter(file)
+    for _,item := range lines {
+        //fmt.Println(item)
+        _, err := file.WriteString(strings.TrimSpace(item) + "\n"); 
+        //file.Write([]byte(item)); 
+        if err != nil {
+            //fmt.Println("debug")
+            fmt.Println(err)
+            break
+        }
+    }
+    return
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
+    var data []string
+    tm := time.Now().Format(time.RFC3339)
+    line := "Time :" + tm
+    data = append(data , line)  
     if r.Method == "POST"{
         body, _ := ioutil.ReadAll(r.Body)
         values, _ := url.ParseQuery(string(body))
         for v , k := range values{
-            fmt.Println(v , strings.Join(k, ""))
+            line = v + " : " + strings.Join(k, "")
+            data = append(data , line)
         }
     }
     if r.Method == "GET"{
         r.ParseForm() 
-        for k, v := range r.Form {
-            fmt.Println(v , strings.Join(k, ""))
+        for v , k := range r.Form {
+            line = v + " : " + strings.Join(k, "")
+            data = append(data , line)
         }
 
     }
+    sdata, _ := readLines("in.txt")
+    for _ , line := range data{
+        sdata = append(sdata , line)
+    }
+    sdata = append(sdata , "--")
+    _ = writeLines(sdata, "in.txt")
+ 
 }
 
 func main() {
-	http.HandleFunc("/showoff", home) 
-	http.HandleFunc("/vlc/status", statusHandler)
-	http.HandleFunc("/vlc/status.asc", status_ascHandler)
-	log.Fatal(http.ListenAndServe(":80", nil))
-
+    // You shouldn't use the root URL this way
+    http.HandleFunc("/", home) 
+    http.HandleFunc("/home", homepage) 
+    http.HandleFunc("/vlc/status", statusHandler)
+    http.HandleFunc("/vlc/status.asc", status_ascHandler)
+    log.Fatal(http.ListenAndServe(":80", nil))
 }
-
