00ddb6707801499986729d269cff8b32c81510bc
Marco Younan <xmarcoied@gmail.com>
2017-06-17 16:17:26 +0100

    Extracting routing functions

--- main.go
+++ main.go
@@ -1,83 +1,28 @@
 package main
 
 import (
-    "net/http"
-    "strings"
-    "log"
-    "io/ioutil"
-    "net/url"
-    "time"
-    "html/template"
-    "database/sql"
-    _ "github.com/lib/pq"
-
-    "updater/util"
-)
-type Page struct{
-    Body []string
-}
-func homepage(w http.ResponseWriter, r *http.Request) {
-    data, _ := util.ReadLines("in.txt") 
-    t, _ := template.ParseFiles("templates/home.html")
-    p := Page{Body : data}
-    t.Execute(w , p)
+	"github.com/jinzhu/gorm"
+       _"github.com/lib/pq"
     
-}
-func statusHandler(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "client/static/status")
-    log.Println("status")
-}
-func status_ascHandler(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "client/static/status.asc")
-    log.Println("status.asc")
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-    var data []string
-    tm := time.Now().Format(time.RFC3339)
-    line := "Time :" + tm
-    data = append(data , line)  
-    if r.Method == "POST"{
-        body, _ := ioutil.ReadAll(r.Body)
-        values, _ := url.ParseQuery(string(body))
-        for v , k := range values{
-            line = v + " : " + strings.Join(k, "")
-            data = append(data , line)
-        }
-    }
-    if r.Method == "GET"{
-        r.ParseForm() 
-        for v , k := range r.Form {
-            line = v + " : " + strings.Join(k, "")
-            data = append(data , line)
-        }
-
-    }
-    sdata, _ := util.ReadLines("in.txt")
-    for _ , line := range data{
-        sdata = append(sdata , line)
-    }
-    sdata = append(sdata , "--")
-    _ = util.WriteLines(sdata, "in.txt")
- 
+	"updater/router"
+)
+type Update_Request struct{
+	gorm.Model
+	ID          uint   `gorm:"primary_key"`
+	Os 	 		string 	   
+	Os_ver 		string
+	Os_arch 	string 
+	Vlc_ver 	string
 }
 
 func main() {
-    psqlInfo := "host=localhost dbname=updater user=updater password=updater sslmode=disable"
-    db, err := sql.Open("postgres", psqlInfo)
-    if err != nil {
-        panic(err)
-    }
-    defer db.Close()
-    
-    err = db.Ping()
-    if err != nil {
-        panic(err)
-    }
-    
-    http.HandleFunc("/", home) 
-    http.HandleFunc("/home", homepage) 
-    http.HandleFunc("/vlc/status", statusHandler)
-    http.HandleFunc("/vlc/status.asc", status_ascHandler)
-    log.Fatal(http.ListenAndServe(":80", nil))
+	// TODO : Move this to Model/database package
+	// TODO : Move the psqlinfo to config
+	psqlInfo := "host=localhost dbname=updater user=postgres password=postgres sslmode=disable"
+	db , _ := gorm.Open("postgres" , psqlInfo)
+	defer db.Close()
+  	db.AutoMigrate(&Update_Request{})
+  	db.LogMode(true)
+
+    	server.StartServer()
 }
