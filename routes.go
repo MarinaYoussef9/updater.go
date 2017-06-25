c12031df0ae1baef2aabe9002c64b3110eabe81b
Marco Younan <xmarcoied@gmail.com>
2017-06-25 13:51:23 +0200

    add channels routing

--- routes.go
+++ routes.go
@@ -1,40 +1,39 @@
-package model
+package main
 
 import (
-	//	"log"
-	"github.com/jinzhu/gorm"
-	_ "github.com/lib/pq"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"updater/model"
 )
 
-type Update_Request struct {
-	//gorm.Model
-	//ID          uint   `gorm:"primary_key"`
-	OS      string `form:"os"`
-	OS_VER  string `form:"os_ver"`
-	OS_ARCH string `form:"os_arch"`
-	VLC_VER string `form:"vlc_ver"`
-}
-type Impl struct {
-	DB *gorm.DB
+func showoff(c *gin.Context) {
+	var requests []model.Update_Request
+	requests = db.AllRequests(requests, c.Param("channel"))
+
+	for _, request := range requests {
+		fmt.Fprint(c.Writer, "ID :", request.ID)
+		fmt.Fprint(c.Writer, " , created_at :", request.Created_At)
+		fmt.Fprint(c.Writer, " , OS :", request.OS)
+		fmt.Fprint(c.Writer, " , OS_VER :", request.OS_VER)
+		fmt.Fprint(c.Writer, " , OS_ARCH :", request.OS_ARCH)
+		fmt.Fprint(c.Writer, " , VLC_VER :", request.VLC_VER)
+		fmt.Fprintln(c.Writer, " , IP :", request.IP)
+	}
+
 }
 
-func (i *Impl) ConnectDB() {
-	// TODO : Move the psqlinfo to config & handle config/yml
-	psqlInfo := "host=localhost dbname=marcoied user=postgres password=postgres sslmode=disable"
-	i.DB, _ = gorm.Open("postgres", psqlInfo)
-	i.DB.LogMode(true)
-	i.DB.AutoMigrate(&Update_Request{})
-}
+func update(c *gin.Context) {
+	// Request params are now getting in GET params
+	var request model.Update_Request
+	c.Bind(&request)
+	log.Println(request)
+	request.Channel = c.Param("channel")
+	// TODO : DB Model API
+	// FIXME : initiate the DB once and pass it everywhere
+	db.NewRequest(request)
+	status_route := "http://update.videolan.org/vlc/" + c.Param("channel") + "/status"
+	c.Redirect(http.StatusMovedPermanently, status_route)
 
-func (i *Impl) NewRequest(r Update_Request) {
-	i.DB.Create(r)
-}
-
-func (i *Impl) AllRequests(r []Update_Request) []Update_Request {
-	i.DB.Find(&r)
-	return r
-}
-
-func (i *Impl) CloseDB() {
-	i.DB.Close()
 }
